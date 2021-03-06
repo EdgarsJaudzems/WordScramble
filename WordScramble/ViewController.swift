71ec686de5d9faefb62d0e678b732b795a746f9c
Edgars Jaudzems <jaudzems.edgars@inbox.lv>
2021-03-06 14:05:37 +0200

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Edgars Jaudzems on 06/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
